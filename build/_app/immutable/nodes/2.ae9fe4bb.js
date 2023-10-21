a7006eb7cce06361c735de0e3361b5f96508f44b
Antoine-LB <antoine.stc25@gmail.com>
2023-10-21 20:33:28 +0200

    new svelte buidl

---
+++ build/_app/immutable/nodes/2.ae9fe4bb.js
@@ -0,0 +1,5 @@
+import{s as m,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as c,i as f,g as p,m as v,h as _,y as u,n as x,a as l,f as r}from"../chunks/index.a21d6cee.js";import{b as d}from"../chunks/paths.61ab77e7.js";function h(i){let e,s=`<a href="${d}/">Home</a>`,n;return{c(){e=p("nav"),e.innerHTML=s,n=v(`
+
+svelte 2`)},l(t){e=_(t,"NAV",{"data-svelte-h":!0}),u(e)!=="svelte-1ynntqc"&&(e.innerHTML=s),n=x(t,`
+
+svelte 2`)},m(t,o){l(t,e,o),l(t,n,o)},p:a,i:a,o:a,d(t){t&&(r(e),r(n))}}}class b extends c{constructor(e){super(),f(this,e,null,h,m,{})}}export{b as component};
