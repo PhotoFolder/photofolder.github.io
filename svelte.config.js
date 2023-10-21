f8a0ba252e9bc99b74ca9f079a001985eec79a5b
Antoine-LB <antoine.stc25@gmail.com>
2023-10-21 20:45:56 +0200

    push to docs

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ pages: 'docs' }),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/photofolder' : ''
 		}
