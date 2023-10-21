69fda7fa4146dfd9d48f93f1c2f961494efe1743
Antoine-LB <antoine.stc25@gmail.com>
2023-10-22 01:15:16 +0200

    tailwind not working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,16 @@
 </script>
 
 <nav>
-	<a href="{base}/">Home</a>
+	<!-- <a href="{base}/">Home</a> -->
 	<!-- <a href="{base}/about">About</a> -->
 </nav>
+
+<div>
+	<div>
+		<h1 class="text-3xl font-bold underline">h1</h1>
+		<p>parargragm</p>
+	</div>
+	<div><img src="{base}/logo.svg" alt="PhotoFolder logo" /></div>
+</div>
 
 <a href="https://forms.gle/UPmNKp9FncKYnvcW7" target="_blank">Contact</a>
