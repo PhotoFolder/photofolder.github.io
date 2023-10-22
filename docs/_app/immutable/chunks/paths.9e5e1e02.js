8264d5e17809a19d41eeb6269655ead27601e993
Antoine-LB <antoine.stc25@gmail.com>
2023-10-22 10:44:06 +0200

    about page buidl

---
+++ docs/_app/immutable/chunks/paths.9e5e1e02.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_i6hg4z)==null?void 0:s.base)??"/photofolder";var e;const a=((e=globalThis.__sveltekit_i6hg4z)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ docs/_app/immutable/nodes/3.eefcae53.js
@@ -0,0 +1,1 @@
+import{s,n as e}from"../chunks/scheduler.e108d1fd.js";import{S as r,i,m as c,n as m,a as p,f as l}from"../chunks/index.a21d6cee.js";function u(o){let t;return{c(){t=c("about page")},l(a){t=m(a,"about page")},m(a,n){p(a,t,n)},p:e,i:e,o:e,d(a){a&&l(t)}}}class _ extends r{constructor(t){super(),i(this,t,null,u,s,{})}}export{_ as component};
