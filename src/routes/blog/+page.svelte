2ee6b9728e3bb422bb10a2a4b1d1363283a64aa2
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2023-11-30 22:32:35 +0100

    deploy

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,29 +1,36 @@
 <script lang="ts">
-	import type { PageData } from '../$types';
+  import type { PageData } from "../$types";
 
-	import PageHead from '$lib/components/PageHead.svelte';
-	import Article from '$lib/components/Article.svelte';
-	import ArticleTitle from '$lib/components/ArticleTitle.svelte';
-	import ArticleMeta from '$lib/components/ArticleMeta.svelte';
-	import ArticleDescription from '$lib/components/ArticleDescription.svelte';
+  import PageHead from "$lib/components/PageHead.svelte";
+  import Article from "$lib/components/Article.svelte";
+  import ArticleTitle from "$lib/components/ArticleTitle.svelte";
+  import ArticleMeta from "$lib/components/ArticleMeta.svelte";
+  import ArticleDescription from "$lib/components/ArticleDescription.svelte";
 
-	export let data: PageData;
+  export let data: PageData;
 </script>
 
-<PageHead title="Home" description="An awesome blog about development with Svelte" />
+<PageHead
+  title="Home"
+  description="An awesome blog about development with Svelte"
+/>
 
-<p>
-	This is a minimalistic example of a blog built with <a href="https://kit.svelte.dev">SvelteKit</a>
-	and <a href="https://mdsvex.com/">MDsveX</a>.
-	<a href="https://github.com/mvasigh/sveltekit-mdsvex-blog">View source code on Github.</a>
-</p>
+<nav>
+  <div class=" flex max-w-4xl m-auto items-center">
+    <img src="/photofolder-logo.svg" alt="logo" class="w-20 md:w-24" />
+
+    <p class="text-3xl md:text-5xl drop-shadow-lg px-4 font-bold text-[#333]">
+      PhotoFolder Blog
+    </p>
+  </div>
+</nav>
 
 {#each data.posts as { slug, title, author, description, date }}
-	<Article>
-		<ArticleTitle {slug} {title} />
-		<ArticleMeta {author} {date} />
-		<ArticleDescription {description} {slug} />
-	</Article>
+  <Article>
+    <ArticleTitle {slug} {title} />
+    <ArticleMeta {author} {date} />
+    <ArticleDescription {description} {slug} />
+  </Article>
 {/each}
 
 <slot />
